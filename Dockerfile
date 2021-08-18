9d5b896598e59a2e13e1654a4f2d8f100353128e
shubhamdwivedii <shubham00d@gmail.com>
2021-08-18 21:38:23 +0530

    added some notes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,89 @@
-FROM golang:alpine 
+# base-image:tag 
+FROM golang:alpine
+# Try to choose the smallest possible image size
+
+# copies everything in current directory to /app in container. 
 COPY . /app  
+# Files listed in .dockerignore will be ignored. 
+
+# To copy only specific files 
+# COPY package.json /app
+
+# A trailing forward slash is needed in destination (/app/) if multiple files are copied. 
+# COPY package.json package-lock.json /app/
+
+# Or use a pattern (will copy any matching file)
+# COPY package*.json /app 
+
+# sets workign directory to /app in container 
 WORKDIR /app
-CMD go run ./server/server.go
+
+# Destination paths are relative to WORKING DIRECTORY
+# COPY README.md . 
+
+# Use this syntax for file names with spaces (last element is destination)
+# COPY ["hello world.txt", "."]
+
+# Add is similar to COPY with additional features 
+# ADD http://somedomain.com/file.json .  
+# Like fetching files from URL or uncompressing zip. 
+# ADD file.zip .
+# Contents will be extracted into WORKDIR 
+
+
+# The RUN commands executes any commands that are normally run in terminal 
+# RUN npm install 
+# RUN apt install python 
+
+# Use ENV to se environment variables 
+ENV API_URL=http://api.myapp.com/ 
+
+# EXPOSE describes what port this container will be listening to. Exposes port for mapping later. 
 EXPOSE 8080
+
+# To set the current user (default is root). 
+# USER app 
+# All the following commands will be run by this user. Preceeding commands will still be run as default user (root)
+
+# User app must be already created (can use RUN to create one before this)
+# RUN addgroup app && adduser -S -G app app 
+
+
+# You can run command in container by starting it like: 
+# > docker run image-name npm start 
+# OR 
+# > docker run go-docker go run ./server/server.go 
+
+# To avoid specifying run command everytime we run contianer, use CMD 
+# With CMD we can supply a default command to be executed. 
+CMD go run ./server/server.go
+# Now we can start container by simply:
+# > docker run go-docker 
+
+# Only one CMD gets executed (last one by default)
+# CMD npm start 
+
+# RUN vs CMD > RUN is excuted when image is built (in intermediate container)
+# > CMD is executed when starting a container (from the built image) 
+# CMD > run-time instruction vs RUN > build-time instruction
+
+# CMD has two forms 
+# 1. Shell form 
+# CMD npm start 
+# Shell form is executed in a separate shell. 
+# Basically: RUN /bin/sh npm start
+
+# 2. Exec form (Prefer this)
+# CMD ["npm", "start"]
+# Exec from executes in current shell. No new shell is spin up for this. 
+
+# CMD can always still be overidden when starting the container: 
+# > docker run go-docker echo hello 
+# "echo hello" will run instead of CMD (npm start)
+
+# ENTRYPOINT is similar to CMD except it cannot be easily overridden
+# ENTRYPOINT ["npm",  "start"]
+
+# To override ENTRYPOINT --entrypoint option needs to be used. 
+# > docker run go-docker --entrypoint echo hello 
+# Avoids accidental overrides I guess. 
--- hello-docker.go
+++ hello-docker.go
@@ -7,16 +7,97 @@ func main() {
 }
 
 // To build image
-// docker build -t go-docker .
+// > docker build -t go-docker .
+
+// To build image and add a tag
+// > docker build -t image-name:image-tag .
+// > docker build -t go-docker:1.2 .
+
+// To add tag later
+// > docker image tag image-name:latest
+
+// multiple tags can be added to same image
+// > docker image tag image-name:first-tag image-name:second-tag
+
+// To switch tag to different version of image
+// > docker image tag image-id image-name:latest
+
+// NOTE image-name is Repository.
+// A Repository can have multiple images with differenct image-ids
+// An image with image-id can have multiple tags. (like latest, v1.3)
 
 // To run image
-// docker run go-docker
+// > docker run go-docker
+
+// To run image with a command (command is run when container is launched)
+// > docker run node-docker npm start
 
 // To pull and run image directly (from dockerhub)
-// docker pull golang/go-image
-// docker images
-// docker run golang/go-image
+// > docker pull golang/go-image
+// > docker images
+// > docker run golang/go-image
 
 // TO expose and map port
-//  docker run -d -p 8080:8080 --name my-container go-docker
+// > docker run -d -p 8080:8080 --name my-container go-docker
 // my-container is name of container go-docker is image name
+
+// To see all images
+// > docker images
+// > docker image ls
+
+// To see all running containers
+// > docker ps
+
+// To see all containers
+// > docker ps -a
+
+// To run in interactive mode (opens shell)
+// > docker run -it image-name
+// > docker run -it image-name bash
+// > docker run -it image-name sh
+
+// To see image history and all its layers
+// > docker history image-name
+// Layers are cached and reused when images are rebuilt
+
+// NOTE: use COPY . /app after running COPY package.json and RUN npm start
+// If image is rebuilt after changes in src, cache is not used for any commands after COPY . /app
+// This way node_modules will still be reused if there are no changes in package.json
+// This will speed up the builds
+
+// To delete danglings images (unused image layers)
+// > docker image prune
+
+// To remove a specific image(s)
+// > docker image rm image-name
+// > docker image rm first-image second-image third-image
+
+// > docker image remove image-name:img-tag
+// If image has multiple tags, only the tag gets remove (otherwise the whole image)
+
+// To delete stopped containers
+// > docker container prune
+
+// To stop a container
+// > docker container kill container-tag
+// > docker container kill container-name
+
+// To push image to dockerhub
+// > docker login
+// > docker push username/image-name:tag
+// > docker push username/repository:tag
+
+// image-name is same as repository.
+
+// Add username to image as:
+// > docker image tag image-id username/image-name:tag-name
+
+// By default latest tag is used automatically.
+
+// To save image as compressed file
+// > docker image save -o image-name.tar image-name:tag
+// > docker image save -o my-image.zip image-name:latest
+
+// To load a saved image
+// > docker image load -i image-name.tar
+// > docker image load -i my-image.zip
