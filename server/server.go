27bc5e74c9c6db9d91170c5fb961e13032eb711e
shubhamdwivedii <shubham00d@gmail.com>
2021-08-22 21:15:21 +0530

    integrated mysql db

--- server/server.go
+++ server/server.go
@@ -7,23 +7,29 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"database/sql" 
 	"strconv"
 	"strings"
 	"sync"
+
+	_ "github.com/go-sql-driver/mysql" 
 )
 
-// Start reading this from main(), then return here.
-
 type Product struct {
-	Name  string  `json:"name"`
+	Id int `json:"id"`
+	Name string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
-type Products []Product
-
 type ProductHandler struct {
 	sync.Mutex
-	products Products
+	db *sql.DB 
+}
+
+func newProductHandler(db *sql.DB) *ProductHandler { // Kinda like a constructor
+	return &ProductHandler{
+		db: db, 
+	}
 }
 
 // Implements Handler Interface (see below)
@@ -45,27 +51,50 @@ func (ph *ProductHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (ph *ProductHandler) get(w http.ResponseWriter, r *http.Request) {
 	defer ph.Unlock()
 	ph.Lock()
-
 	id, err := idFromUrl(r)
 	if err != nil {
-		respondWithJSON(w, http.StatusOK, ph.products)
-		return // still need to return after sending response.
+		// Return all products 
+		results, err := ph.db.Query("select * from products")
+		if err != nil {
+			log.Fatal("Error when fetching products from table", err.Error())
+		}
+		defer results.Close() 
+
+		var products []Product 
+		for results.Next() {	
+			var product Product
+			err = results.Scan(&product.Id, &product.Name, &product.Price) 
+			if err != nil {
+				respondWithError(w, http.StatusInternalServerError, err.Error())
+				return 
+			}
+			products = append(products, product)
+		}
+		respondWithJSON(w, http.StatusOK, products)
+		return 
 	}
-	if id >= len(ph.products) || id < 0 {
-		respondWithError(w, http.StatusNotFound, "not found")
-		return
+	
+	// returning product with id 
+	var product Product 
+	qry := fmt.Sprintf("Select * from products where id = %v", id)
+	err = ph.db.QueryRow(qry).Scan(&product.Id, &product.Name, &product.Price) 
+	// Unmarshall supports &product but not Scan WHY???
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, err.Error())
+		return 
 	}
-	respondWithJSON(w, http.StatusOK, ph.products[id])
+	respondWithJSON(w, http.StatusOK, product)
 
 }
+
 func (ph *ProductHandler) post(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()                // It is a good practise to close the Body once you are done. Google whY??
-	body, err := ioutil.ReadAll(r.Body) // Reads all from a Reader (r.Body implements Reader interface)
+	defer r.Body.Close()             
+	body, err := ioutil.ReadAll(r.Body)
 	// body here is a []byte
 
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error()) // err.Error() returns the message string
-		return                                                           // always return after responding.
+		respondWithError(w, http.StatusInternalServerError, err.Error()) 
+		return // always return after responding.
 	}
 	contentType := r.Header.Get("content-type")
 	if contentType != "application/json" {
@@ -74,16 +103,28 @@ func (ph *ProductHandler) post(w http.ResponseWriter, r *http.Request) {
 	}
 	var product Product
 	err = json.Unmarshal(body, &product) // converts a []byte json string to a Product
-	/* json.Unmarshal(data []byte, v interface{}) error
-	Parses JSON-encoded data and stores the result in the value pointed to by v (pointer)
-	*/
+	
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 	defer ph.Unlock() // defered functions go in a STACK. execution is Last-In-First-Out
 	ph.Lock()
-	ph.products = append(ph.products, product)
+
+	// Adding product to sql table 
+	create_product, err := ph.db.Prepare("INSERT INTO products (name, price) VALUES (?, ?)")
+	if err != nil {
+		fmt.Println("Errrr", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return 
+	} 
+	_, err = create_product.Exec(product.Name, product.Price)
+	if err != nil {
+		fmt.Println("Error::", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return 
+	} 
+	fmt.Println("Product created", product)
 	respondWithJSON(w, http.StatusCreated, product)
 }
 
@@ -91,15 +132,14 @@ func (ph *ProductHandler) put(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	id, err := idFromUrl(r)
 	if err != nil {
-		respondWithError(w, http.StatusNotFound, err.Error())
+		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 	body, err := ioutil.ReadAll(r.Body) // Reads all from a Reader (r.Body implements Reader interface)
-	// body here is a []byte
-
+	
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error()) // err.Error() returns the message string
-		return                                                           // always return after responding.
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return                                                          
 	}
 	contentType := r.Header.Get("content-type")
 	if contentType != "application/json" {
@@ -107,48 +147,88 @@ func (ph *ProductHandler) put(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var product Product
-	err = json.Unmarshal(body, &product) // Takes cares of type validation also (to some extent)
+	err = json.Unmarshal(body, &product)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
+
+	fmt.Println("Updating product", product)
+
+	// Updating product 
 	defer ph.Unlock()
 	ph.Lock()
 
-	// length might change if not locked.
-	if id >= len(ph.products) || id < 0 {
-		respondWithError(w, http.StatusNotFound, "not found")
-		return
+	// Check if product exists
+	var origP Product 
+	qry := fmt.Sprintf("Select * from products where id = %v", id)
+	err = ph.db.QueryRow(qry).Scan(&origP.Id, &origP.Name, &origP.Price) 
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, err.Error())
+		return 
 	}
 
 	if product.Name != "" {
-		ph.products[id].Name = product.Name
+		origP.Name = product.Name
 	}
 	if product.Price != 0.0 {
-		ph.products[id].Price = product.Price
+		origP.Price = product.Price
 	}
-	respondWithJSON(w, http.StatusCreated, ph.products[id])
+
+	// Updating Product
+	update_product, err := ph.db.Prepare(`UPDATE products SET name = ?, price = ? WHERE id = ?`)
+	if err != nil {
+		fmt.Println("Errrr", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return 
+	} 
+	
+	_, err = update_product.Exec(origP.Name, origP.Price, origP.Id)
+	if err != nil {
+		fmt.Println("Error::", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return 
+	} 
+	
+	fmt.Println("Product updated", origP)
+	respondWithJSON(w, http.StatusCreated, origP)
 }
+
 func (ph *ProductHandler) delete(w http.ResponseWriter, r *http.Request) {
 	id, err := idFromUrl(r)
 	if err != nil {
-		respondWithError(w, http.StatusNotFound, "not found")
+		respondWithError(w, http.StatusBadRequest, "not found")
 		return
 	}
 
 	defer ph.Unlock()
 	ph.Lock()
 
-	if id >= len(ph.products) || id < 0 {
-		respondWithError(w, http.StatusNotFound, "not found")
-		return
+	// Check if product exists
+	var origP Product 
+	qry := fmt.Sprintf("Select * from products where id = %v", id)
+	err = ph.db.QueryRow(qry).Scan(&origP.Id, &origP.Name, &origP.Price) 
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, err.Error())
+		return 
 	}
 
-	if id < len(ph.products)-1 { // if not the last product then switch with the last product.
-		ph.products[len(ph.products)-1], ph.products[id] = ph.products[id], ph.products[len(ph.products)-1] // Trick to switch last and ith elemetn // BASIC GO PATTERN
-	}
-
-	ph.products = ph.products[:len(ph.products)-1] // from 0 upto (not including) last index
+	// deleting product 
+	delete_product, err := ph.db.Prepare(`DELETE FROM products WHERE id = ?`)
+	if err != nil {
+		fmt.Println("Errrr", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return 
+	} 
+	
+	_, err = delete_product.Exec(origP.Id)
+	if err != nil {
+		fmt.Println("Error::", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return 
+	} 
+	
+	fmt.Println("Product deleted", origP)
 	respondWithJSON(w, http.StatusNoContent, "")   // It is convention to not return anything for DELETE request
 }
 
@@ -181,21 +261,79 @@ func idFromUrl(r *http.Request) (int, error) {
 
 }
 
-func newProductHandler() *ProductHandler { // Kinda like a constructor // BASIC GO PATTERN
-	return &ProductHandler{
-		products: Products{
-			Product{"Shoes", 25.00},
-			Product{"Webcam", 50.00},
-			Product{"Mic", 20.00},
-		},
+func initDB() *sql.DB {
+	DB_URL := "root:admin@tcp(127.0.0.1:3306)/test"
+
+	URLS := strings.Split(DB_URL, "/")
+	
+	CONNECTION_URL := URLS[0]
+	DATABASE_NAME := URLS[1]
+
+	fmt.Println("Connection:", CONNECTION_URL)
+	fmt.Println("Database:", DATABASE_NAME)
+
+	// Add sql driver by: "go get github.com/go-sql-driver/mysql"
+	fmt.Println("Drivers:", sql.Drivers())
+
+	db, err := sql.Open("mysql", CONNECTION_URL + "/")
+	if err != nil {
+		log.Fatal("Unable to open connection to DB", err.Error())
+	} else {
+		fmt.Println("Connected To DB...")
 	}
+
+	_,err = db.Exec("USE " + DATABASE_NAME)
+	if err != nil {
+		fmt.Println("DATABASE NOT EXISTS", DATABASE_NAME, err.Error())
+		_,err = db.Exec("CREATE DATABASE "+ DATABASE_NAME)
+		if err != nil {
+			log.Fatal("Error: Creating Database", err.Error())
+		} else {
+			fmt.Println("Database Created Successfully...")
+		}
+	} else {
+		fmt.Println("Database found...")
+	}
+
+	return db 
 }
 
+func checkTable(table string, db *sql.DB) error {
+	results, err := db.Query("select * from products")
+	if err != nil {
+		fmt.Println("Table", table, "does not exists")
+		create_table, err := db.Prepare("CREATE TABLE products(id int NOT NULL AUTO_INCREMENT, name varchar(30), price int, PRIMARY KEY (id))")
+		if err != nil {
+			fmt.Println("Error: Creating Table Statement")
+			return err 
+		} else {
+			_,err := create_table.Exec() 
+			if err != nil {
+				fmt.Println("Error: Creating Table:")
+				return err 
+			} else {
+				fmt.Println("Table Created successfully")
+				return nil 
+			}
+		}
+	} else {
+		fmt.Println("Table", table, "Found...")
+		results.Close()
+		return nil  
+	}
+} 
+
 func RunServer() {
-	// SQL_CONNECTION := "root:admin@tcp(127.0.0.1:3306)/test"
+	db := initDB()
+	err := checkTable("products", db)
+	if err != nil {
+		log.Fatal("Error: Table products:", err.Error())
+	}
+
+	defer db.Close() // TO make sure DB connection is closed properly
 
 	port := ":8080"
-	ph := newProductHandler()
+	ph := newProductHandler(db)
 	http.Handle("/products", ph)
 	http.Handle("/products/", ph) // "/products" and "/products/" are handled differently
 
